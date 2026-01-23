d555698367562d639227f87868ff3023ffba72aa
redtext <100484201+realredtext@users.noreply.github.com>
2026-01-23 13:11:15 -0500

    Update Slowchat.js

--- Slowchat.js
+++ Slowchat.js
@@ -6,7 +6,7 @@ chatBtn.innerText = "Chat";
 chatBtn.onclick = () => {
     //a prompt is used to make this as slow as possible, this is SLOWchat for a reason
     let msg = prompt("Send:");
-    api_chat_send(msg)
+    api_chat_send(msg);
 };
 
 const upperChat = byId("chat_upper");
