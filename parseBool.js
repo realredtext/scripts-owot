e1c16e228b8876cc8b3716a4ea91944463abd321
redtext <100484201+realredtext@users.noreply.github.com>
2026-01-23 13:03:10 -0500

    throwing this in the trash dir soon

--- parseBool.js
+++ parseBool.js
@@ -2,7 +2,7 @@ function parseBool(str) {
     str+="";
     str = str.toLowerCase();
     
-    if(!(str in {"true":1,"false": 0})) return;
+    if(!(str in {"true":1,"false": 0})) throw new TypeError("Invalid string!");
     
     return str === "true";
 };
