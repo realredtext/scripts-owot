95e519440784d36e5a3f0e97fbc6411b513ebce5
redtext <100484201+realredtext@users.noreply.github.com>
2026-01-23 13:00:42 -0500

    made not terrible

--- short_hexcode.js
+++ short_hexcode.js
@@ -1,4 +1,7 @@
-let int_to_short_hexcode = (val) => {
-    val = val % 4096; //#1000 made impossible
+function intToShortHex(val) {
+    if(typeof val !== "number") return;
+    if(val > 4096) val = 4096;
+    if(val < 0) val = 0;
+    if(val % 1) val = Math.floor(val);
     return `#${val.toString(16).padStart(3, 0)}`
-}
+};
