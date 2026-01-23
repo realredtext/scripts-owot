dd27d7ac7e479dc080ece592f6b24ea1843dbc4f
redtext <100484201+realredtext@users.noreply.github.com>
2026-01-23 13:12:11 -0500

    trash?

--- FPChat.js
+++ FPChat.js
@@ -1,5 +1,5 @@
 function FPChat(message) {
-    addChat(null, 6955, "user", "fp", message, "fp", true, true, true, "#33f", getDate())
+    addChat(null, 6955, "user", "fp", message, "fp", true, true, true, "#33f", getDate());
 };
 
 client_commands.fpchat = (args) => {
